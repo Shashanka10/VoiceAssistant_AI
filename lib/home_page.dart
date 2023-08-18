c95c304f6fdfc70d8ef2b33917cb19b34aa5e859
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-08-18 13:30:54 +0545

    added voice and image recog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
+import "package:speech_to_text/speech_recognition_result.dart";
+import "package:speech_to_text/speech_to_text.dart";
 import "package:voiceassist_ai/feature_box.dart";
+import "package:voiceassist_ai/openai_service.dart";
 import "package:voiceassist_ai/pallete.dart";
 
 class Homepage extends StatefulWidget {
@@ -10,6 +13,43 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  final OpenAIService openAIService = OpenAIService();
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,102 +58,114 @@ class _HomepageState extends State<Homepage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          // virtual assistant profile
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 160,
-                  width: 160,
-                  margin: const EdgeInsets.only(top: 8),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // virtual assistant profile
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 160,
+                    width: 160,
+                    margin: const EdgeInsets.only(top: 8),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 164,
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
                     shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/virtualAssistant.png"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Good Morning! What task can I do for you?",
+                  style: TextStyle(
+                    fontFamily: "Cera Pro",
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
                   ),
                 ),
               ),
-              Container(
-                height: 164,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/virtualAssistant.png"),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-              top: 30,
             ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor,
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 20,
               ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                "Good Morning! What task can I do for you?",
+              child: const Text(
+                "Here are a few features",
                 style: TextStyle(
-                  fontFamily: "Cera Pro",
+                  fontFamily: 'Cera Pro',
                   color: Pallete.mainFontColor,
-                  fontSize: 25,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(
-              top: 10,
-              left: 20,
-            ),
-            child: const Text(
-              "Here are a few features",
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: "ChatGPT",
-                desctext:
-                    "Be smart, think smart and act smart, to keep things organized",
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: "Dall-E",
-                desctext:
-                    "Perfect AI image you could ask for, be creative and get inspired",
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: "Smart Voice Assistant",
-                desctext:
-                    "Get the best of both worlds with a voice assistant powered by AI",
-              ),
-            ],
-          )
-        ],
+            const Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: "ChatGPT",
+                  desctext:
+                      "Be smart, think smart and act smart, to keep things organized",
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Dall-E",
+                  desctext:
+                      "Perfect AI image you could ask for, be creative and get inspired",
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: "Smart Voice Assistant",
+                  desctext:
+                      "Get the best of both worlds with a voice assistant powered by AI",
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Color.fromARGB(255, 227, 175, 192),
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            final speech = await openAIService.isArtPromptAPI(lastWords);
+            print(speech);
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        backgroundColor: const Color.fromARGB(255, 227, 175, 192),
         foregroundColor: const Color.fromARGB(255, 6, 26, 42),
         splashColor: const Color.fromARGB(255, 136, 190, 200),
         child: const Icon(Icons.mic),
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
